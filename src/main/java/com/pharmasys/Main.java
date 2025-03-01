a9a7486f4173de10e8c805d0992f71632224ef62
morrisonslue <chris@morrisonslue.com>
2025-03-01 20:01:50 -0330

    Updated Main.java, added Doctor.java, removed old Person.class

--- src/main/java/com/pharmasys/Main.java
+++ src/main/java/com/pharmasys/Main.java
@@ -1,4 +1,4 @@
-package main.java.com.pharmasys;
+package com.pharmasys;
 
 public class Main {
     public static void main(String[] args) {
---
+++ src/main/java/com/pharmasys/models/Doctor.java
@@ -0,0 +1,32 @@
+package com.pharmasys.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Doctor extends Person {
+    private String specialization;
+    private List<Patient> patients;
+
+    public Doctor(String id, String name, int age, String phoneNumber, String specialization) {
+        super(id, name, age, phoneNumber);
+        this.specialization = specialization;
+        this.patients = new ArrayList<>();
+    }
+
+    // add patient
+    public void addPatient(Patient patient) {
+        patients.add(patient);
+    }
+
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    public List<Patient> getPatients() {
+        return patients;
+    }
+
+    public String toString() {
+        return super.toString() + " | Specialization: " + specialization + " | Patients: " + patients.size();
+    }
+}
